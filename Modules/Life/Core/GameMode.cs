e0cb89df0cc015033519242bedae1184ccfb4800
Valentin <valentinlamothe56@gmail.com>
2019-07-20 09:39:05 +0200

    Gamemode modifications

--- Modules/Life/Core/GameMode.cs
+++ Modules/Life/Core/GameMode.cs
@@ -4,7 +4,36 @@ using UnityEngine;
 
 namespace Life.Core
 {
-    class GameMode
+    using UnityEngine;
+
+    public class Gamemode : MonoBehaviour
     {
+        /// <summary>
+        /// Called when a player connect
+        /// </summary>
+        /// <param name="playerId">The netId of the connected player</param>
+        public virtual void OnPlayerConnect(uint playerId) { }
+        /// <summary>
+        /// Called when a player disconnect
+        /// </summary>
+        /// <param name="playerId">The netId of the disconnected player</param>
+        public virtual void OnPlayerDisconnect(uint playerId) { }
+        /// <summary>
+        /// Called when a player spawn
+        /// </summary>
+        /// <param name="playerId">The netId of the spawned player</param>
+        public virtual void OnPlayerSpawn(uint playerId) { }
+
+        /// <summary>
+        /// Called when a vehicle respawns
+        /// </summary>
+        /// <param name="vehicleId">The netId of the vehicle</param>
+        public virtual void OnVehicleSpawn(uint vehicleId) { }
+        /// <summary>
+        /// Called when a vehicle is destroyed - either by exploding or becoming submerged in water.
+        /// </summary>
+        /// <param name="vehicleId">The netId of the vehicle</param>
+        public virtual void OnVehicleDeath(uint vehicleId) { }
     }
+
 }
